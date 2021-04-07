fbcad1fedeb7ad348f2ccf2cfc36e7104ad59b9b
Mateo Marin <mateomarin977@gmail.com>
2021-04-07 09:08:55 +0200

    drag and reorder list items

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,31 @@
+function newItem() {
 
-  function newItem(){
-
-  //javascript
-  //1. Adding a new item to the list of items:
+//Adding a new item to the list of items:
   let li = $('<li></li>');
   let inputValue = $('#input').val();
-  li.append(inputValue);
+  li.append(document.createTextNode(inputValue));
 
   if (inputValue === '') {
-    alert("You must write something!");
+    alert('You must write something')
   } else {
-    $('#list').append(li);
+    let list = $('#list');
+    list.append(li);
   }
-  ///2. Crossing an item out:
-    function crossOut() {
-  		li.toggleClass("strike");
-  	}
 
-  	li.on("dblclick", function crossOut() {
-  		li.toggleClass("strike");
-  	});
+//Crossing out an item from the list of items:
+  li.on('dblclick', function(){
+  li.toggleClass('strike');
+  });
 
-   //2. Crossing out an item from the list of items:
-     function crossOut() {
-   		li.classList.toggle("strike");
-   	}
+// Adding the delete button "X":
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+li.append(crossOutButton);
+crossOutButton.append(document.createTextNode('X'));
+crossOutButton.on('click', function(){
+  li.addClass('delete');
+});
 
-   	li.addEventListener("dblclick",crossOut);
+//Reordering the items:
+  $('#list').sortable();
 
-   //3(i). Adding the delete button "X":
-   let crossOutButton = $('<crossOutButton></crossOutButton>');
-   crossOutButton.append(document.createTextNode('X'));
-   li.append(crossOutButton);
-   crossOutButton.on("click", deleteListItem);
-  //   function deleteListItem(){
-  // 		li.addClass("delete")
-  // 	}
-   $('#list').sortable();
-   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-     function deleteListItem(){
-   		li.classList.add("delete")
-   	}
-   // 4. Reordering the items:
-     $('#list').sortable();
-
-  }
+}
